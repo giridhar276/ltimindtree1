a7b9765feb9ffaf1c70684c81b85335dda486431
giridhar276 <giridhar276@gmail.com>
2025-09-25 15:59:34 +0530

    Create task4.py

---
+++ pythonprograms/task4.py
@@ -0,0 +1,16 @@
+'''
+write a program to replace all the vowels in a string with *.
+
+Enter a string: python
+pyth*n
+'''
+
+text = input("Enter a string :")  #python
+vowels = "aeiouAEIOU"
+result = ""
+for char in text:
+    if char in vowels:
+        result = result + "*"
+    else:
+        result = result + char
+print("Output:", result)
